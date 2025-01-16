60ef57537317a3ad20e006e882902dfce21c557c
ShinYounHo <suh2963@gmail.com>
2025-01-16 14:08:49 +0900

    pathVariable

--- src/main/java/com/example/demo/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/example/demo/domain/wiseSaying/controller/WiseSayingController.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.wiseSaying.entity.WiseSaying;
 import com.example.demo.domain.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,13 +30,8 @@ public class WiseSayingController {
         return wiseSayingService.write("명언1", "작가1");
     }
 
-    @GetMapping("/wiseSayings/1")
-    public WiseSaying getItem1() {
-        return wiseSayingService.getItem(1).orElse(null);
-    }
-
-    @GetMapping("/wiseSayings/2")
-    public WiseSaying getItem2() {
-        return wiseSayingService.getItem(2).orElse(null);
+    @GetMapping("/wiseSayings/{id}")
+    public WiseSaying getItem(@PathVariable int id) {
+        return wiseSayingService.getItem(id).orElse(null);
     }
 }
