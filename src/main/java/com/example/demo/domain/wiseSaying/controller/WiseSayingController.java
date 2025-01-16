26ef19c0d914d6d0e6b745398934caf1b0fe6c44
ShinYounHo <suh2963@gmail.com>
2025-01-16 12:21:34 +0900

    bean, component, autowired

--- src/main/java/com/example/demo/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/example/demo/domain/wiseSaying/controller/WiseSayingController.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.wiseSaying.controller;
 
 import com.example.demo.domain.wiseSaying.entity.WiseSaying;
 import com.example.demo.domain.wiseSaying.service.WiseSayingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,20 @@ import java.util.List;
 @RestController
 public class WiseSayingController {
 
-    WiseSayingService wiseSayingService = new WiseSayingService();
+    private WiseSayingService wiseSayingService;
+
+    @Autowired
+    public WiseSayingController(WiseSayingService wiseSayingService) {
+        this.wiseSayingService = wiseSayingService;
+    }
 
     @GetMapping("/wiseSayings")
     public List<WiseSaying> getWiseSayings() {
         return wiseSayingService.getAllItems();
+    }
+
+    @GetMapping("/wiseSaying/write")
+    public WiseSaying write(String content, String author) {
+        return wiseSayingService.write("명언1", "작가1");
     }
 }
--- src/main/java/com/example/demo/domain/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/example/demo/domain/wiseSaying/service/WiseSayingService.java
@@ -1,14 +1,36 @@
 package com.example.demo.domain.wiseSaying.service;
 
 import com.example.demo.domain.wiseSaying.entity.WiseSaying;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class WiseSayingService {
 
-    List<WiseSaying> wiseSayingList = new ArrayList<>();
-    private int lastId = 0;
+    List<WiseSaying> wiseSayingList;
+    private int lastId;
+
+    public WiseSayingService() {
+        this.wiseSayingList = new ArrayList<>();
+        this.lastId = 0;
+
+        WiseSaying w1 = WiseSaying.builder()
+                .id(++lastId)
+                .content("삶이 있는 한 희망은 있다.")
+                .author("키케로")
+                .build();
+
+        WiseSaying w2 = WiseSaying.builder()
+                .id(++lastId)
+                .content("나에게 죽음을 알리지 말라.")
+                .author("이순신")
+                .build();
+
+        wiseSayingList.add(w1);
+        wiseSayingList.add(w2);
+    }
 
     public List<WiseSaying> getAllItems() {
         return wiseSayingList;
